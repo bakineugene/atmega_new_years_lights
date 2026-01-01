fc35ac74cdedcb3d46c6f6cb2a2f98fab3c3237f
Eugene Bakin <bakin.eugene@ya.ru>
2026-01-01 20:54:09 +0500

    9 wave modes

--- main.c
+++ main.c
@@ -21,10 +21,12 @@
 #define WDT_PREPARE_CHANGE() WDTCR = (1 << WDCE)
 #define WDT_ENABLE_INTERRUPT_250() WDTCR = (1 << WDTIE) | (1 << WDP2)
 
+volatile uint8_t mode = 1;
+
 ISR(PCINT0_vect) {
     PCINT0_DISABLE();
 
-    // CHANGE SMTH
+    if (++mode > 9) mode = 1;
 
     cli();
     WDT_PREPARE_CHANGE();
@@ -40,13 +42,13 @@ ISR(WDT_vect) {
 
 #define WAVE_LEN 24
 
-const uint8_t wave_r[WAVE_LEN] PROGMEM = {
+const uint8_t wave_hard[WAVE_LEN] PROGMEM = {
     0,  5, 15, 30, 60, 100, 150, 200,
   240,255,255,240,220,200,170,140,
   110, 80, 55, 35, 20, 10,  5,  0
 };
 
-const uint8_t wave_g[WAVE_LEN] PROGMEM = {
+const uint8_t wave_soft[WAVE_LEN] PROGMEM = {
     0,  0,  2,  5, 10,  20,  40,  80,
   120,160,180,160,140,110, 80,  50,
    30, 15,  8,  3,  1,  0,  0,  0
@@ -70,11 +72,42 @@ int main(void) {
     while (1) {
         for (int l = 0; l < 100; ++l) {
             int idx = (l + step) % WAVE_LEN;
-            led.sendRGB(
-                pgm_read_byte(&wave_g[idx]),
-                pgm_read_byte(&wave_r[idx]),
-                0
-            );
+            uint8_t green = 0;
+            uint8_t red = 0;
+            uint8_t blue = 0;
+
+            switch (mode) {
+                case 1:
+                case 4:
+                case 7:
+                    green = pgm_read_byte(&wave_soft[idx]);
+                    break;
+                case 2:
+                case 5:
+                case 8:
+                    red = pgm_read_byte(&wave_soft[idx]);
+                    break;
+                case 3:
+                case 6:
+                case 9:
+                    blue = pgm_read_byte(&wave_soft[idx]);
+                    break;
+            }
+            switch (mode) {
+                case 4:
+                case 6:
+                    red = pgm_read_byte(&wave_hard[idx]);
+                    break;
+                case 7:
+                case 5:
+                    blue = pgm_read_byte(&wave_hard[idx]);
+                    break;
+                case 8:
+                case 9:
+                    green = pgm_read_byte(&wave_hard[idx]);
+                    break;
+            }
+            led.sendRGB(red, green, blue);
         }
         if (step >= WAVE_LEN - 1) step = 0;
         else ++step;
